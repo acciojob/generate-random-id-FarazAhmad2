582421705c9da8838f9f03cec594a19d3b4309b4
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-03-03 15:51:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function makeid(l) {
   // write your code here
+	const char_list = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let res = '';
+
+  for (let i = 0; i < l; i++) {
+    res += char_list.charAt(Math.floor(Math.random() * char_list.length));
+  }
+  return res;
 }
 
 // Do not change the code below.
